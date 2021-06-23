34138c079d812994fbaa2b5005f81392d81069bd
mabsboza <marcosboza@moi-solutions.com>
2021-06-22 21:07:48 -0600

    Add Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: "white", marginBotton: "20px" }}>
-          <DiCode size="3rem"/><Span>GM</Span>
+          <DiCode size="3rem"/><Span>GMR</Span>
         </a>
       </Link>
     </Div1>
