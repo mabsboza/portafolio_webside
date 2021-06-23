4bddf89b0f98ef68dbb5c05635821a7415e4820e
mabsboza <marcosboza@moi-solutions.com>
2021-06-22 21:10:46 -0600

    Add footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,39 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer,
+        SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="505 8787-7975">505 8787-7975</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:mabsboza@gmail.com">mabsboza@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating and making life easier</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/mabsboza">
+            <AiFillGithub size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href="https://www.instagram.com/marcosbozasaldivar/">
+            <AiFillInstagram size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/marcos-boza-sald%C3%ADvar-276a1637/">
+            <AiFillLinkedin size="3rem"/>
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
